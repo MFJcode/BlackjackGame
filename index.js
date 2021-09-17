36f893c232fb91baf432ed423046a71754482e15
MFJcode <mfjcode@gmail.com>
2021-09-17 18:03:53 +0200

    Display the sum

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ let isAlive = true;
 let message = "";
 
 let messageEL = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
     if (sum <= 20) {
+        sumEl.textContent = "Sum:" +" "+ sum;
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
+        sumEl.textContent = "Sum:" +" "+ sum;
         message = "You've got Blackjack!";
         hasBlackJack = true;
     } else {
+        sumEl.textContent = "Sum:" +" "+ sum;
         message = "You're out of the game!";
         isAlive = false;
     }
