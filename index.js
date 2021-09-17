fd3d8df73691c62b55edfc7868659745f31d8105
MFJcode <mfjcode@gmail.com>
2021-09-17 17:38:11 +0200

    Display the message

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
+let messageEL = document.getElementById("message-el");
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂";
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳";
+        message = "You've got Blackjack!";
         hasBlackJack = true;
     } else {
-        message = "You're out of the game! 😭";
+        message = "You're out of the game!";
         isAlive = false;
     }
 
-    console.log(message);
+    messageEL.textContent = message;
 }
 
 
